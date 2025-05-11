bba6e06a129f0c68c0bfc165e1d48930c3b044a7
Fco Javier Felix <ffelix@inode64.com>
2025-05-11 11:32:19 +0200

    Include sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import robotsTxt from 'astro-robots-txt';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://inode64.com',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [robotsTxt(), sitemap()]
 });
