e6bcd21e4d3fe113d874994b251a76eae5060e94
Fco Javier Felix <ffelix@inode64.com>
2025-05-19 21:50:54 +0200

    include support for htaccess

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import robotsTxt from 'astro-robots-txt';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+import htaccessIntegration from "astro-htaccess";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://inode64.com',
+  site: "https://inode64.com",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [robotsTxt(), sitemap(), mdx()]
+  integrations: [
+    robotsTxt(),
+    sitemap(),
+    mdx(),
+    htaccessIntegration({
+      generateHtaccessFile: import.meta.env.GENERATE_HTACCESS_FILE === "true",
+      errorPages: [{ code: 404, document: "/404.html" }],
+      redirects: [{ match: /^\/tienda/, url: "https://www.npmjs.com/package/astro-htaccess" }],
+    }),
+  ],
 });
