1cb51a03c70862a35eaa43c9b25cfa4c2924ce49
Fco Javier Felix <ffelix@inode64.com>
2025-05-20 15:39:17 +0200

    Include support for RSS

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,19 @@
+// src/pages/rss.xml.ts
+import { siteConfig } from "@/config/site-config";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export async function GET() {
+  const posts = await getCollection("blog");
+
+  return rss({
+    title: "Inode64's Blog",
+    description: "Últimos artículos y noticias",
+    site: "https://inode64.com",
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
+}
